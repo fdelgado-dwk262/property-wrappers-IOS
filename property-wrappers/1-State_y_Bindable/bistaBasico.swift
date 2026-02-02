17c109498107db5558ca5adebe20a1910721a685
Fernando <dwk262@gmail.com>
2026-02-02 10:45:57 +0100

    Inicio de Observer

--- property-wrappers/1-State_y_Bindable/bistaBasico.swift
+++ property-wrappers/1-State_y_Bindable/bistaBasico.swift
@@ -5,12 +5,13 @@
 //  Created by Equipo 9 on 2/2/26.
 //
 
-// importante importar la libreria de observable
+// importante importar la libreria de observable es opcional ya que lo implementa el siguiente import
 import Observation
 // importación clasica
 import SwiftUI
 
 // Iniciamos el ejemplop con una clase
+@Observable
 class ContadorViewModel {
     var count: Int = 0
     var bame: String = "Contador App"
@@ -22,14 +23,16 @@ class ContadorViewModel {
 
 struct bistaBasico: View {
 
+    // instanciamos sobre la clase usando state
+    // hace dos cosas
+    // inicializamos y poseer el objeto
+    // nos sirve para:
+    // - para que no se creen nuevas instancias de la clase entre actualizaciones de vistas
+    // - para acceder a los bindings ($)
+    @State private var viewModel: ContadorViewModel = ContadorViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+
     }
 }
 
