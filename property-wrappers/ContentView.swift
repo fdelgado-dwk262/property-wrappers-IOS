a824f72d6b33330abb5e59ec252d9320abe8e46f
Fernando <dwk262@gmail.com>
2026-02-02 10:09:02 +0100

    Initial Commit

---
+++ property-wrappers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  property-wrappers
+//
+//  Created by Equipo 9 on 2/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ property-wrappers/property_wrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  property_wrappersApp.swift
+//  property-wrappers
+//
+//  Created by Equipo 9 on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct property_wrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
